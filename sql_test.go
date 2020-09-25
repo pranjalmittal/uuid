9d31e3a7ad5a19d5bedd22a89750875bcfe3f949
Pranjal Mittal <pranjalmittal603@gmail.com>
2020-09-25 11:03:12 +0000

    tests for null uuid

--- sql_test.go
+++ sql_test.go
@@ -111,3 +111,44 @@ func TestValue(t *testing.T) {
 		t.Error("Value() did not return expected string")
 	}
 }
+
+func TestNullUUIDValueNil(t *testing.T) {
+	u := NullUUID{}
+
+	val, _ := u.Value()
+	if val != nil {
+		t.Error("Value() did not return expected value")
+	}
+}
+
+func TestNullUUIDScanValid(t *testing.T) {
+	u := UUID{0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}
+	s1 := "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
+
+	u1 := NullUUID{}
+	err := u1.Scan(s1)
+	if err != nil {
+		t.Error("unable to scan null uuid")
+	}
+	if u1.Valid != true {
+		t.Error("null uuid not valid")
+	}
+	if u1.UUID != u {
+		t.Error("uuid are not same")
+	}
+}
+
+func TestNullUUIDScanNil(t *testing.T) {
+	u := NullUUID{UUID{0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}, true}
+
+	err := u.Scan(nil)
+	if err != nil {
+		t.Error("unable to scan null uuid")
+	}
+	if u.Valid != false {
+		t.Error("null uuid not valid")
+	}
+	if u.UUID != Nil {
+		t.Error("")
+	}
+}
